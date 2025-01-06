d145707ea8558639683ea2f34f2e0e03597dc73e
dmc-tools <135308970+dmc-tools@users.noreply.github.com>
2025-01-06 15:45:21 +0530

    changes done

--- src/json/feature.ts
+++ src/json/feature.ts
@@ -48,7 +48,7 @@ export const features = [
     featureHeading: "Performance Marketing",
     features: [
       {
-        id: 6,
+        id: 1,
         featureName: "Google Ads",
         featureLogo: "/features/google-ads.png",
         featuresIcon: "/features/features-icons/google-ads.png",
@@ -56,7 +56,7 @@ export const features = [
           "Effortlessly track the daily performance of your Google Ads campaigns with our performance analysis tool. DM Cockpit Makes It Easy To Stay On Top Of Your Keyword Rankings With 100% Accurate Tracking.",
       },
       {
-        id: 7,
+        id: 2,
         featureName: "Meta Ads",
         featureLogo: "/features/meta-ads.png",
         featuresIcon: "/features/features-icons/meta-ads.png",
@@ -69,7 +69,7 @@ export const features = [
     featureHeading: "Social Media Management",
     features: [
       {
-        id: 8,
+        id: 1,
         featureName: "Facebook Insights & Management Tool",
         featureLogo: "/features/facebook-insights-management-tool.png",
         featuresIcon:
@@ -78,7 +78,7 @@ export const features = [
           "Simplify. Engage. Excel - Master Your Facebook Strategy with DM Cockpit. DM Cockpit Makes It Easy To Stay On Top Of Your Keyword Rankings With 100% Accurate Tracking.",
       },
       {
-        id: 9,
+        id: 2,
         featureName: "Instagram Insights And Scheduling Tool",
         featureLogo: "/features/instagram-insights-and-scheduling-tool.png",
         featuresIcon:
@@ -87,7 +87,7 @@ export const features = [
           "Crack the Instagram Code: Engagement & Analytics Made Easy. DM Cockpit Makes It Easy To Stay On Top Of Your Keyword Rankings With 100% Accurate Tracking.",
       },
       {
-        id: 10,
+        id: 3,
         featureName: "LinkedIn Reporting Tool",
         featureLogo: "/features/linkedin-reporting-tool.png",
         featuresIcon: "/features/features-icons/linkedin-reporting-tool.png",
@@ -100,7 +100,7 @@ export const features = [
     featureHeading: "Reporting",
     features: [
       {
-        id: 11,
+        id: 1,
         featureName: "Google Analytics",
         featureLogo: "/features/google-analytics.png",
         featuresIcon: "/features/features-icons/google-analytics.png",
@@ -108,7 +108,7 @@ export const features = [
           "Dive deep into website metrics, user engagement, conversion data & Unlock the power of data with our dedicated Google Analytics reporting tool. DM Cockpit Makes It Easy To Stay On Top Of Your Keyword Rankings With 100% Accurate Tracking.",
       },
       {
-        id: 12,
+        id: 2,
         featureName: "Google Search Console",
         featureLogo: "/features/google-search-console.png",
         featuresIcon: "/features/features-icons/google-search-console.png",
